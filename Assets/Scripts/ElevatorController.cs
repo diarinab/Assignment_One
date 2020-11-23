fe4d0a38a9508fb23afb93371ef300f7dde0843f
DianaBoltersdorf <diana.boltersdorf@edu.fh-joanneum.at>
2020-11-23 19:38:53 +0100

    elevator_try_1

---
+++ Assets/Scripts/ElevatorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ElevatorController : MonoBehaviour
+{
+    [SerializeField] private float elevationSpeed = 10f;
+    [SerializeField] private Transform[] wayPoints = null;
+    
+    private NavMeshAgent agent;
+    private Vector3 startingPosition;
+    
+    private int wayPointIndex = 0;
+
+    protected virtual Vector3 GetNextDestination()
+    {
+        Vector3 destination = wayPoints[wayPointIndex].position;
+
+        wayPointIndex = (wayPointIndex + 1) % wayPoints.Length;
+
+        return destination;
+    }
+    private void Start()
+    {
+        startingPosition = gameObject.transform.position;
+    }
+    
+   
+}
--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -8,7 +8,7 @@ public class Rotation : MonoBehaviour
     [SerializeField] private float rotationSpeed = 10f;
 
 
-    void Update()
+    private void Update()
     {
         gameObject.transform.Rotate(0,rotationSpeed*Time.deltaTime, 0);
     }
