b1f55071221f6ba45c5f9f324e709b665f455249
DianaBoltersdorf <diana.boltersdorf@edu.fh-joanneum.at>
2020-11-23 19:04:06 +0100

    coins

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 10f;
+
+
+    void Update()
+    {
+        gameObject.transform.Rotate(0,rotationSpeed*Time.deltaTime, 0);
+    }
+}
